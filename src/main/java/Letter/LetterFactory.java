703ebb3f5ab4cc29fbd1404230af88d110e9d8d9
Gregoire Marcoux <gregoire.marcoux@polymtl.ca>
2024-02-04 15:17:23 -0500

    Ca genere pas les lettres, mais ca plante moins quavant

--- src/main/java/Letter/LetterFactory.java
+++ src/main/java/Letter/LetterFactory.java
@@ -18,7 +18,8 @@ public final class LetterFactory {
     public static BaseShape create_A()  {
         Ellipse letter = new Ellipse(halfMaxWidth, halfMaxHeight);
         Ellipse middleToRemove = new Ellipse((maxWidth - stripeThickness)/2, (maxHeight - stripeThickness)/2);
-        Rectangle littleALeg = new Rectangle(halfStripeThickness, maxHeight).translate(new Point2d(halfMaxWidth-halfStripeThickness/2, 0.0));
+        Rectangle littleALeg = new Rectangle(halfStripeThickness, maxHeight);
+        littleALeg.translate(littleALeg.getCoords(), new Point2d(halfMaxWidth-halfStripeThickness/2, 0.0));
         letter.remove(middleToRemove);
         letter.add(littleALeg);
         return letter;
@@ -29,13 +30,14 @@ public final class LetterFactory {
      * @return BaseShape containing the letter B
      */
     public static BaseShape create_B() {
-        BaseShape letter = new Circle(maxHeight/4).translate(new Point2d(0.0, -0.25*maxHeight));
-        letter.add(new Circle(maxHeight/4).translate(new Point2d(0.0, 0.25*maxHeight)));
-        letter.remove(new Circle(maxHeight/4 - halfStripeThickness).translate(new Point2d(0.0, -0.25*maxHeight)));
-        letter.remove(new Circle(maxHeight/4 - halfStripeThickness).translate(new Point2d(0.0, 0.25*maxHeight)));
-        letter.remove(new Rectangle(maxWidth, maxHeight).translate(new Point2d(-maxWidth/2, 0.0)));
-        letter.add(new Rectangle(halfStripeThickness, maxHeight));
-        return letter;
+        //BaseShape letter = new Circle(maxHeight/4);
+        //letter.translate(letter.getCoords(), new Point2d(0.0, -0.25*maxHeight));
+        //letter.add(new Circle(maxHeight/4).translate(new Point2d(0.0, 0.25*maxHeight)));
+        //letter.remove(new Circle(maxHeight/4 - halfStripeThickness).translate(new Point2d(0.0, -0.25*maxHeight)));
+        //letter.remove(new Circle(maxHeight/4 - halfStripeThickness).translate(new Point2d(0.0, 0.25*maxHeight)));
+        //letter.remove(new Rectangle(maxWidth, maxHeight).translate(new Point2d(-maxWidth/2, 0.0)));
+        //letter.add(new Rectangle(halfStripeThickness, maxHeight));
+        return null;
     }
 
     /** TODO
@@ -45,7 +47,8 @@ public final class LetterFactory {
     public static BaseShape create_C() {
         Ellipse letter = new Ellipse(halfMaxWidth, halfMaxHeight);
         Ellipse middleToRemove = new Ellipse((maxWidth - stripeThickness)/2, (maxHeight - stripeThickness)/2);
-        BaseShape rightToRemove = new Rectangle(stripeThickness, maxHeight/1.5).translate(new Point2d(halfMaxWidth-halfStripeThickness, 0.0));
+        BaseShape rightToRemove = new Rectangle(stripeThickness, maxHeight/1.5);
+        rightToRemove.translate(rightToRemove.getCoords(), new Point2d(halfMaxWidth-halfStripeThickness, 0.0));
         letter.remove(middleToRemove);
         letter.remove(rightToRemove);
         return letter;
@@ -59,7 +62,8 @@ public final class LetterFactory {
         Ellipse letter = new Ellipse(maxWidth/2, maxHeight/2);
         Ellipse middleToRemove = new Ellipse((maxWidth - stripeThickness)/2, (maxHeight - stripeThickness)/2);
         Rectangle middleStripe = new Rectangle(maxWidth, stripeThickness);
-        Rectangle smallRemoval = new Square(stripeThickness).translate(new Point2d(halfMaxWidth-halfStripeThickness, stripeThickness));
+        Rectangle smallRemoval = new Square(stripeThickness);
+        smallRemoval.translate(smallRemoval.getCoords(), new Point2d(halfMaxWidth-halfStripeThickness, stripeThickness));
         letter.remove(middleToRemove);
         letter.remove(smallRemoval);
         letter.add(middleStripe);
@@ -72,7 +76,9 @@ public final class LetterFactory {
      */
     public static BaseShape create_H() {
         Rectangle verticalBar = new Rectangle(halfStripeThickness, maxHeight);
-        Rectangle horizontalBar = new Rectangle(maxWidth, stripeThickness).translate(new Point2d(0.0, -halfMaxHeight));
+        Rectangle verticalBar2 = new Rectangle(halfStripeThickness, maxHeight);
+        verticalBar2.translate(verticalBar2.getCoords(), new Point2d(maxWidth, 0.0));
+        Rectangle horizontalBar = new Rectangle(maxWidth, stripeThickness); //.translate(new Point2d(0.0, -halfMaxHeight));
         BaseShape letterH = new BaseShape();
         letterH.add(verticalBar);
         letterH.add(horizontalBar);
@@ -85,14 +91,17 @@ public final class LetterFactory {
      */
     public static BaseShape create_N() {
         Circle letter = new Circle(halfMaxWidth);
-        letter.translate(new Point2d(0.0, -0.25*maxHeight));
+        letter.translate(letter.getCoords(), new Point2d(0.0, -0.25*maxHeight));
         Circle insideBit = new Circle(halfMaxWidth-halfStripeThickness);
-        insideBit.translate(new Point2d(0.0, -0.25*maxHeight));
+        insideBit.translate(insideBit.getCoords(), new Point2d(0.0, -0.25*maxHeight));
         letter.remove(insideBit);
-        Rectangle halfToRemove = new Rectangle(maxWidth, 0.75*maxHeight).translate(new Point2d(0.0, 0.25*maxHeight/2));
+        Rectangle halfToRemove = new Rectangle(maxWidth, 0.75*maxHeight);
+        halfToRemove.translate(halfToRemove.getCoords(), new Point2d(0.0, 0.25*maxHeight/2));
         letter.remove(halfToRemove);
-        Rectangle leftLeg = new Rectangle(halfStripeThickness, maxHeight).translate(new Point2d(-halfMaxWidth+halfStripeThickness/2, 0.0));
-        Rectangle rightLeg = new Rectangle(halfStripeThickness, 0.75*maxHeight).translate(new Point2d(halfMaxWidth-halfStripeThickness/2, 0.25*maxHeight/2));
+        Rectangle leftLeg = new Rectangle(halfStripeThickness, maxHeight);
+        leftLeg.translate(leftLeg.getCoords(), new Point2d(-halfMaxWidth+halfStripeThickness/2, 0.0));
+        Rectangle rightLeg = new Rectangle(halfStripeThickness, 0.75*maxHeight);
+        rightLeg.translate(rightLeg.getCoords(), new Point2d(halfMaxWidth-halfStripeThickness/2, 0.25*maxHeight/2));
         letter.add(leftLeg);
         letter.add(rightLeg);
         return letter;
